849082cbe0c13e03b571f406a2d80b0af75ebbf6
Madhav Parikh <parikh.madhav1999@gmail.com>
2021-02-19 21:07:20 +0530

    Added Security

--- app/config.py
+++ app/config.py
@@ -2,13 +2,15 @@ import sqlite3
 from sqlite3 import Error
 from datetime import datetime,timedelta
 from flask import g,sessions
-
+import secrets
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # print(basedir)
 class Config(object):
-    TESTING = True
-    DEBUG = True
+    TESTING = False
+    DEBUG = False
     FLASK_ENV='production'
 
-    SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
+    secret_key = secrets.token_hex(32)
+    SECRET_KEY = secret_key
+    PERMANENT_SESSION_LIFETIME =  timedelta(minutes=30)
