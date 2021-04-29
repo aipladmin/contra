5199b229a7d007d39335ddefe7c1dd5aa27f3201
Madhav Parikh <parikh.madhav1999@gmail.com>
2021-04-29 14:46:36 +0530

    API Changes

--- app/controller/auth/api_test.py
+++ app/controller/auth/api_test.py
@@ -1,8 +1,9 @@
 import requests
 
-BASE = "http://contradashboard.online"
+# BASE = "http://contradashboard.online"
+BASE = "127.0.0.1:8000"
 
-response = requests.post(BASE+"/contra",{'name':'qwerty','phone':'views','email':'likes'})
+response = requests.post(BASE+"/contra",{'name':'qwerty','phone':9998564854,'email':'likes','password':'qwe'})
 print(response.json())
 
 # response = requests.get(BASE+"/contraEP2")
