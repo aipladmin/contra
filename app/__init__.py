d1fec4ad042f176762e9dc70f3f53b4f7423a9d5
Madhav Parikh <parikh.madhav1999@gmail.com>
2021-05-03 00:58:09 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,17 @@ def create_app():
  
  
     
+    app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Contra Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON 
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
+    """ app.json_encoder = Config.MyJSONEncoder """
 
     mail = Mail()
     app.config['MAIL_SERVER']='smtp.gmail.com'
